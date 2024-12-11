3fe391b7103e7495d6268f55cbd9d2e0ee367fd2
Abimbola Jolayemi <abimbolajolayemi111@yahoo.com>
2024-12-11 11:05:09 +0100

    removed example.js file

--- example.js
+++
@@ -1,9 +0,0 @@
-const expression = "3 + 5 * 2"; // Example expression
-const tokens = expression.match(/(\d+\.?\d*|\+|\-|\*|\/)/g); // Tokenizing the expression
-// tokens would be ["3", "+", "5", "*", "2"]
-
-let result1 = parseFloat(tokens[0]);
-let result2 = parseFloat(tokens[1]);
-let result3 = parseFloat(tokens[2]);
-let result4 = parseFloat(tokens[3]);
-console.log(result1, result2, result3, result4);
